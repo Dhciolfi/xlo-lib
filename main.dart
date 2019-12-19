0fd6fd3e4cd7a59b783a0480a2ee4f97aa5cdeb9
Dhciolfi <daniel.ciolfi@gmail.com>
2019-12-19 20:41:03 -0300

    0 - Codigo Inicial

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'XLO',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
